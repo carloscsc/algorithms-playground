2c4ed157e2065563996a2ed52e8fc19a945afc5f
carloscsc <carlos.azarro@gmail.com>
2025-12-12 13:42:08 -0300

    feat: implement MinStack class with push, pop, top, and getMin methods

--- src/01-arrays/3-stacks/exercicies.test.ts
+++ src/01-arrays/3-stacks/exercicies.test.ts
@@ -51,9 +51,132 @@ describe("Stack tests", () => {
 			return stack.length === 0;
 		};
 
-		// expect(isValid("[]")).toBe(true);
-		// expect(isValid("([{}])")).toBe(true);
-		// expect(isValid("[(])")).toBe(false);
+		expect(isValid("[]")).toBe(true);
+		expect(isValid("([{}])")).toBe(true);
+		expect(isValid("[(])")).toBe(false);
 		expect(isValid("()[]{}")).toBe(true);
+	});
+});
+
+describe("Minimum Stack - my solution + stack of test", () => {
+	it("Should design a minimun stack", () => {
+		class MinStack {
+			private stack: number[] = [];
+			private min: number[] = [];
+
+			constructor() {}
+
+			/**
+			 * @param {number} val
+			 * @return {void}
+			 */
+			push(val: number) {
+				this.stack.push(val);
+
+				if (this.min.length === 0) {
+					this.min.push(val);
+				} else if (val <= this.min[this.min.length - 1]!) {
+					this.min.push(val);
+				} else {
+					this.min.push(this.min[this.min.length - 1]!);
+				}
+			}
+
+			/**
+			 * @return {void}
+			 */
+			pop() {
+				if (this.stack.length === 0) return;
+				this.stack.pop();
+				this.min.pop();
+			}
+
+			/**
+			 * @return {number}
+			 */
+			top() {
+				return this.stack.length > 0
+					? this.stack[this.stack.length - 1]
+					: this.stack[0];
+			}
+
+			/**
+			 * @return {number}
+			 */
+			getMin() {
+				return this.min.length > 0
+					? this.min[this.min.length - 1]
+					: this.min[0];
+			}
+		}
+
+		function minimunStackTest(input: unknown[]): unknown[] {
+			const output: unknown[] = [];
+			const stack = new MinStack();
+
+			for (let i = 0; i < input.length; i++) {
+				switch (input[i]) {
+					case "MinStack":
+						output.push(null);
+						break;
+
+					case "push":
+						stack.push(input[i + 1] as number);
+						output.push(null);
+						break;
+
+					case "getMin":
+						output.push(stack.getMin());
+						break;
+
+					case "pop":
+						stack.pop();
+						output.push(null);
+						break;
+
+					case "top":
+						output.push(stack.top());
+						break;
+
+					default:
+						break;
+				}
+			}
+
+			return output;
+		}
+
+		expect(
+			minimunStackTest([
+				"MinStack",
+				"push",
+				1,
+				"push",
+				2,
+				"push",
+				0,
+				"getMin",
+				"pop",
+				"top",
+				"getMin",
+			]),
+		).toStrictEqual([null, null, null, null, 0, null, 2, 1]);
+
+		expect(
+			minimunStackTest([
+				"MinStack",
+				"push",
+				-2,
+				"push",
+				-2,
+				"push",
+				-3,
+				"push",
+				-3,
+				"getMin",
+				"pop",
+				"getMin",
+			]),
+		).toStrictEqual([null, null, null, null, null, -3, null, -3]);
 	});
 });
